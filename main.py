5fe466791ab789e6ce1421527c1ace977edfe8e3
Gabriel Simoes <gmachadosimoes@gmail.com>
2020-12-15 21:11:37 +0000

    Updated project

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, url_for
+import string
+import secrets
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    password = ''
+    if request.method == 'GET':
+        password = pwd_gen()
+    return render_template("index.html", password=password)
+
+def pwd_gen():
+    alphabet = string.ascii_letters + string.punctuation + string.digits
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(10))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and any((c in string.punctuation) for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+
+    return password
+
+app.run()
